e7c48a4be4ededda2c4d1a711ca2bf83205198df
TTI Édgar Salguero <adrian@taotechideas.com>
2019-03-26 08:46:39 -0500

    Email input for customer in Voucher creation

--- resources/views/vouchers/js/create_edit.blade.php
+++ resources/views/vouchers/js/create_edit.blade.php
@@ -183,6 +183,30 @@ $(document).ready(function(){
                 }
             });
         });
+        $('#customerModal input[id = customer_email]')
+            .on('tokenfield:createtoken', function (e) {
+                var data = e.attrs.value.split('|')
+                e.attrs.value = data[1] || data[0]
+                e.attrs.label = data[1] ? data[0] + ' (' + data[1] + ')' : data[0]
+            })
+            .on('tokenfield:createdtoken', function (e) {
+                var re = /\S+@\S+\.\S+/
+                var valid = re.test(e.attrs.value)
+                if (!valid) {
+                    $(e.relatedTarget).addClass('invalid')
+                }
+            })
+            .on('tokenfield:edittoken', function (e) {
+                if (e.attrs.label !== e.attrs.value) {
+                    var label = e.attrs.label.split(' (')
+                    e.attrs.value = label[0] + '|' + e.attrs.value
+                }
+            })
+            .tokenfield({
+                beautify: false,
+                createTokensOnBlur: true,
+                inputType: 'email',
+            });
     @endif
     $('#customer').change(function() {
         if($(this).val() != '' && $(this).val() != null) {
