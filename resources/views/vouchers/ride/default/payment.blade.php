1cdf03cbe4d1cf8e1f22c307a75ea3e5e55234e8
TTI Édgar Salguero <adrian@taotechideas.com>
2019-04-11 16:35:48 -0500

    Added time unit and term to RIDE

--- resources/views/vouchers/ride/default/payment.blade.php
+++ resources/views/vouchers/ride/default/payment.blade.php
@@ -1,14 +1,18 @@
 <div style="padding: 2px 2px 0px 2px" class="card border-dark mb-3">
     <table class="table table-sm">
         <thead>
-            <th class="align-middle">FORMA DE PAGO</th>
-            <th class="align-middle">VALOR</th>
+            <th class="align-bottom">FORMA DE PAGO</th>
+            <th class="align-bottom"><center>VALOR</center></th>
+            <th class="align-bottom">UNIDAD DE TIEMPO</th>
+            <th class="align-bottom"><center>PLAZO</center></th>
         </thead>
       <tbody>
         @foreach ($voucher->payments as $payment)
             <tr>
               <td class="align-middle">{{ \ElectronicInvoicing\PaymentMethod::find($payment->payment_method_id)->name }}</td>
-              <td class="align-middle">{{ number_format($payment->total, 2, '.', '') }}</td>
+              <td class="text-right align-middle">{{ number_format($payment->total, 2, '.', '') }}</td>
+              <td class="align-middle">{{ \ElectronicInvoicing\TimeUnit::find($payment->time_unit_id)->name }}</td>
+              <td class="text-right align-middle">{{ number_format($payment->term, 2, '.', '') }}</td>
             </tr>
         @endforeach
       </tbody>
