18284a12054927996a51421e674df991580abb5e
TTI Édgar Salguero <adrian@taotechideas.com>
2019-04-26 17:54:03 -0500

    Debug fix when download report (5)

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -389,6 +389,7 @@ class ReportController extends Controller
                 }
                 info('BINARY FILE RESPONSE START');
                 $binaryFileResponse = response()->download('vouchers.zip', 'vouchers.zip', $headers)->deleteFileAfterSend();
+                ob_end_clean();
                 info('END OF OBJECT CREATION AND FUNCTION');
                 return $binaryFileResponse;
                 break;
