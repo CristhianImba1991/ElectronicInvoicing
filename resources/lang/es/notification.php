0130bd8f1189663727c3e6c94c0b72d9be6810ee
TTI Édgar Salguero <adrian@taotechideas.com>
2019-05-22 16:16:51 -0500

    Subject email changed when new voucher is released

--- resources/lang/es/notification.php
+++ resources/lang/es/notification.php
@@ -19,7 +19,6 @@ return [
     'your_user_has_been_created_and_you_can_use_the_following_credentials_to_login' => 'Su usuario ha sido creado y puede usar las siguientes credenciales para iniciar sesión.',
     'for_your_service_questions_and_information_write_an_email_to_infotaotechideascom' => 'Para su servicio, preguntas e información, escriba un correo electrónico a info@taotechideas.com.',
     'email_address_password' => '        Correo electrónico: :email' . "\n" . '        Contraseña: :password',
-    //'new_voucher_from_company_to_customer' => 'NUEVO COMPROBANTE DE :company PARA :customer',
     'new_voucher_number_from_company_to_customer' => '{0} NUEVO :voucher (:number) DE :company PARA :customer|{1} NUEVA :voucher (:number) DE :company PARA :customer',
     'you_can_find_the_voucher_files_in_the_attachments_of_this_email' => 'Puede encontrar los archivos del comprobante en los adjuntos de este correo electrónico, o puede verlos o descargarlos de nuestro sistema haciendo clic en el siguiente enlace.',
 
