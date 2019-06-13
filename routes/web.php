9ef0244061b9dee11f7c2733aee658cc7a8f1900
Ubuntu <ubuntu@ip-192-168-1-83.ec2.internal>
2019-06-13 13:24:07 +0000

    Changes for HTTPS

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
+$proxy_url    = getenv('PROXY_URL');
+$proxy_schema = getenv('PROXY_SCHEMA');
+
+if (!empty($proxy_url)) {
+   URL::forceRootUrl($proxy_url);
+}
+
+if (!empty($proxy_schema)) {
+   URL::forceScheme($proxy_schema);
+}
+
 Route::get('/', function () {
     return view('welcome');
 });
